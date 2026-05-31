6916fd774e17a90ce72ae395b46a872525e3b04e
Artist <satyamshiv55@gmail.com>
2026-06-01 01:09:36 +0530

    Update dir.py

--- Elevenyts/core/dir.py
+++ Elevenyts/core/dir.py
@@ -1,3 +1,18 @@
+# ==========================================================
+# Copyright (c) 2026 ArtistBots
+# All Rights Reserved.
+#
+# Project      : ArtistBots API Telegram Music Bot
+# Powered By   : Artist
+# Type         : API Based Telegram Music Bot
+#
+# Bot          : @ArtistApibot
+# Channel      : https://t.me/artistbots
+# GitHub       : https://github.com/elevenyts
+#
+# Unauthorized copying, modification, or redistribution
+# of this source code without permission is prohibited.
+# ==========================================================
 from pathlib import Path
 
 from Elevenyts import logger
